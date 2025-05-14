36c3e3d3be77f66107adb038821fdc101408e8d5
Mouammar Soule <mouammar8080ni@gmail.com>
2025-05-14 15:01:00 +0200

    "initialisation du projet"

---
+++ m1webApp/src/pages/Home/index.jsx
@@ -0,0 +1,35 @@
+import {useEffect, useState} from 'react';
+import View from "./view.jsx";
+import {getTopGames, getGameCover} from "../../services/api";
+
+export default function Home() {
+    const [games, setGames] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        async function fetchData(){
+            const topGame = await getTopGames();
+
+            const gameEnriched = await Promise.all(
+                topGame.slice(0, 10).map(async (game) => {
+                    if (game.cover?.id) {
+                        const cover = await getGameCover(game.cover.id)
+                        if (cover && cover.url) {
+                            return {
+                                     ...game, coverUrl :cover.url,
+                        };
+
+                    }
+                }
+                return { ...game, coverUrl :null};
+                })
+            );
+            setGames(gameEnriched)
+            setLoading(false);
+        }
+        fetchData();
+    }, []);
+
+    return <View games={games} loading={loading}/>;
+
+}
