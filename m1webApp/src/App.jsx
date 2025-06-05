f7275f7e5cb65fe632bb77d406fc19022959c1b2
Mouammar Soule <mouammar8080ni@gmail.com>
2025-06-05 12:18:32 +0200

    Navbar + Translation

--- m1webApp/src/App.jsx
+++ m1webApp/src/App.jsx
@@ -1,12 +1,15 @@
 import {Routes, Route} from "react-router-dom";
 import Home from "./pages/Home";
 import Register from "./pages/Register/index.jsx";
+import Layout from "./Layout.jsx";
 
 export default function App(){
     return (
         <Routes>
-            <Route path="/home" element={<Home/>}/>
-            <Route path="/register" element={<Register/>}/>
+            <Route element={<Layout/>}>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/register" element={<Register/>}/>
+            </Route>
         </Routes>
     );
 }
