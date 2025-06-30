04898448a8815bab802ec0bb3430a8250d314b9d
Mouammar Soule <mouammar8080ni@gmail.com>
2025-06-30 19:49:49 +0200

    "Jeu et les entreprise affecté"

--- m1webApp/src/pages/GameDetail/index.jsx
+++ m1webApp/src/pages/GameDetail/index.jsx
@@ -1,6 +1,13 @@
 import {useEffect, useState} from "react";
 import View from "./view.jsx";
-import {getScreenshots, getGamesDetail, removeFavorite, addFavorite, fetchMe} from "../../services/api";
+import {
+    getScreenshots,
+    getGamesDetail,
+    removeFavorite,
+    addFavorite,
+    fetchMe,
+    getCompagnieById
+} from "../../services/api";
 import {useParams} from "react-router";
 import useToken from "../../hooks/useToken.js";
 
@@ -10,6 +17,7 @@ export default function GameDetail() {
     const [screenshots, setScreenshots] = useState([]);
     const [loading, setLoading] = useState(true);
     const [isFavorite, setIsFavorite] = useState(false);
+    const [companies, setCompanies] = useState([]);
     const token = useToken();
 
     useEffect(() => {
@@ -25,6 +33,21 @@ export default function GameDetail() {
                 //je verifie si le jeu est dans les favoris
                 const _isFavoris = user.favorites.some(fav => fav.id === parseInt(id));
                 setIsFavorite(_isFavoris);
+
+                //companies details
+                const involvedCompanyIds= gameDetail.involvedCompanies?.ids || [];
+                const companiesData = await Promise.all(
+                    involvedCompanyIds.map(async (companyId) => {
+                        try {
+                            const company = await getCompagnieById(companyId, token);
+                            return company;
+                        } catch (e) {
+                            console.error(`Erreur chargement compagnie ${companyId}`, e);
+                            return null;
+                        }
+                    })
+                );
+                setCompanies(companiesData.filter(c => c !== null));
 
             }catch(error){
                 console.log(error);
@@ -56,6 +79,7 @@ export default function GameDetail() {
         loading={loading}
         isFavorite={isFavorite}
         onToggleFavorite={handleToggleFavorite}
+        companies={companies}
     />
     );
 }
